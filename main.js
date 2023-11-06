ad938cb2cf6c0ebe4d74191babbbe868862d577c
Jesse Granier <jesse.granier@lapiscine.pro>
2023-11-06 12:30:18 +0100

    Commentaires

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-
+// Création d'une variable dans laquelle j'envoie chaque carte "chien" nommée par la classe .card
+// Query Selector All pour les 3 cartes
 const cards = document.querySelectorAll('.card')
 
+// On va chercher dans le tableau pour chaque carte
 cards.forEach((dogsCard) => {
+// On met un EventListener qui réagira au click sur la card
     dogsCard.addEventListener("click", () => {
+// Création d'une variable pour récupérer le carte en display none correspondant à la disponibilité du chien
         const available = dogsCard.querySelector(".dispo")        
+// Boucle if : si la carte est affiché => la cacher, sinon l'afficher
             if (available.style.display == "block") {
              available.style.display = "none"
             } else {
